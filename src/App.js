d6e67af10244052bfb46eefefa218001e82de77c
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-14 13:38:06 +0530

    Main Image with text

--- src/App.js
+++ src/App.js
@@ -12,11 +12,21 @@ function App() {
   return (
     <>
       <Navbar />
+
       <div>
         <div className="full-width-div">
-          <img src={profileimage} className="full-width" />
+          <img src={profileimage} className="profimage" />
+          <div className="peradata">
+            <p>
+              We crush your competitors, goals, and sales records - without the
+              B.S.
+            </p>
+            <button className="buttonstart">Get free consultation</button>
+          </div>
         </div>
       </div>
+
+
       <WebMobileDevelopment />
       <Digital />
 
