e768365a60fd8892d515bedef8bf36e83f370155
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-14 12:49:24 +0530

    image added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import profileimage from "./assets/andy-kelly-0E_vhMVqL9g-unsplash 2.png";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div>
+        <div className="full-width-div">
+          <img src={profileimage} className="full-width" />
+        </div>
+      </div>
       <Footer />
     </>
   );
